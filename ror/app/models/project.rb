6ef00bc1a986e8c09acffe9a35ce5ace18351a20
arturstan <arturstan@gmail.com>
2024-04-07 14:35:31 +0200

    add validation to project's dates

--- ror/app/models/project.rb
+++ ror/app/models/project.rb
@@ -5,5 +5,26 @@ class Project < ApplicationRecord
   belongs_to :goal, optional:true
   validates :name, presence: { message: "Name is required" }
   validates :name, uniqueness: { scope: :user_id, message: "You already have an area with this name" }
+  validate :start_date_after_now
+  validate :due_date_after_start_date
 
+  private
+
+  def start_date_after_now
+    if start_date.nil?
+      return
+    end
+    if start_date < Date.today
+      errors.add(:start_date, "should be after or equal today")
+    end
+  end
+
+  def due_date_after_start_date
+    if start_date.nil? || due_date.nil?
+      return
+    end
+    if due_date < start_date
+      errors.add(:due_date, "should be after than due date")
+    end
+  end
 end
