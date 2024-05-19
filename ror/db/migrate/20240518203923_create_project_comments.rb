9d83d53bd31485bf9c4253a276d851e1d4c03b5e
arturstan <arturstan@gmail.com>
2024-05-19 21:03:34 +0200

    add comment to project, fix model (tbc)

--- ror/db/migrate/20240518203923_create_project_comments.rb
+++ ror/db/migrate/20240518203923_create_project_comments.rb
@@ -2,8 +2,8 @@ class CreateProjectComments < ActiveRecord::Migration[7.1]
   def change
     create_table :project_comments do |t|
       t.text :comment
-      t.references :project_id, null: false, foreign_key: true
-      t.references :user_id, null: false, foreign_key: true
+      t.belongs_to :user, null: false, foreign_key: true
+      t.belongs_to :project, null: false, foreign_key: true
 
       t.timestamps
     end
