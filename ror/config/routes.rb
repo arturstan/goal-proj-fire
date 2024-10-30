27194136562d656560268d81b58a3ef826aae3a8
arturstan <arturstan@gmail.com>
2024-10-30 08:55:02 +0100

    only user from permit list can create account

--- ror/config/routes.rb
+++ ror/config/routes.rb
@@ -7,7 +7,9 @@ Rails.application.routes.draw do
   resources :goals
   resources :tags
   resources :areas
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: 'users/registrations'
+  }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
