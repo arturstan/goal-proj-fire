7ef7708697458f7984238ef5723c69b40d367fbe
arturstan <arturstan@gmail.com>
2024-11-18 17:32:02 +0100

    heroku deploy...

--- ror/app/assets/config/manifest.js
+++ ror/app/assets/config/manifest.js
@@ -1,4 +1,6 @@
+// app/assets/config/manifest.js
+
 //= link_tree ../images
 //= link_directory ../stylesheets .css
 //= link_tree ../../javascript .js
-//= link_tree ../../../vendor/javascript .jss
+//= link_tree ../../../vendor/javascript .js
